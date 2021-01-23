d0795628c78878fb02829522d012a23b6bfef7f3
Undercover Stamp <77801870+IAmUnStTV@users.noreply.github.com>
2021-01-23 12:52:12 +0000

    Update commands.py

--- commands.py
+++ commands.py
@@ -13,4 +13,5 @@ async def ping(ctx):
     fullcmd = 'Time:' + tictoc
     await ctx.send(fullcmd)
 
-bot.run('[OMITTED]') # Note: this line is to hide the API key so random peoople can't destroy the bot.
+bot.run('[OMITTED]') # Note: this line is to hide the API key so \
+random peoople can't destroy the bot. That's what the GitHub repo is for
